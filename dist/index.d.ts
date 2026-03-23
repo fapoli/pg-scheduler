20168f7aeb86e54e7e850f57452a76c2c757f495
Federico Poli <fapoli@gmail.com>
2026-03-23 20:07:40 +0100

    Remove node_modules and dist, add .gitignore

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { startWorker, runWorkerCycle, oneTime, recurring } from "./worker/worker.js";
-export type { WorkerHandle, Logger } from "./worker/worker.js";
-export { maxRetries, exponentialBackoff, fixedDelay } from "./failure-handler.js";
-export type { FailureHandler } from "./failure-handler.js";
-export { scheduleTask, rescheduleTask } from "./postgres/store.js";
--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export { startWorker, runWorkerCycle, oneTime, recurring } from "./worker/worker.js";
-export { maxRetries, exponentialBackoff, fixedDelay } from "./failure-handler.js";
-export { scheduleTask, rescheduleTask } from "./postgres/store.js";
